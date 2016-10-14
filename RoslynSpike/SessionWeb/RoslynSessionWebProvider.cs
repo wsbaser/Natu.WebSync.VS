d0b546f2dadbbebc8347fd5d609863bbcc1166f1
Alexey Volkov <wsbaser@gmail.com>
2016-10-14 08:32:57 +0300

    Removed unused references

--- RoslynSpike/SessionWeb/RoslynSessionWebProvider.cs
+++ RoslynSpike/SessionWeb/RoslynSessionWebProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
-using EnvDTE;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FindSymbols;
 using NLog;
